9c274f5537a67172e92ab8ff992f51f1dc96015f
Aman <abhriguvansh99@gmail.com>
2020-10-30 15:34:45 -0700

    added functions in Calculator.js to help calculate result

--- calculator/src/Components/Calculator/Calculator.js
+++ calculator/src/Components/Calculator/Calculator.js
@@ -11,7 +11,42 @@ class Calculator extends Component {
       result: '',
     };
   }
+  calculate() {
+    try {
+      this.setState({
+        // eslint-disable-next-line
+        result: (eval(this.state.result) || '') + '',
+      });
+    } catch (e) {
+      this.setState({
+        result: 'error',
+      });
+    }
+  }
+  onClick(button) {
+    if (button === '=') {
+      this.calculate();
+    } else if (button === 'C') {
+      this.reset();
+    } else if (button === 'CE') {
+      this.backspace();
+    } else {
+      this.setState({
+        result: this.state.result + button,
+      });
+    }
+  }
+  reset() {
+    this.setState({
+      result: '',
+    });
+  }
 
+  backspace() {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    });
+  }
   render() {
     return (
       <div>
